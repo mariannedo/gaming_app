fd0253b99a35cbc9bf45591fabc2d90bfbf5dcce
mariannegreat <mariannepdo@gmail.com>
2013-11-13 21:26:23 -0500

    Working Rock, Paper, Scissors game (unstyled)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,40 @@
 class GamesController < ApplicationController
-  def index
-  	@games = ["Secret Number"]
-  end
+	def index
+	end
+
+	def rock_paper_scissors 
+	end
+
+	def rock_paper_scissors_play
+		app_guess = [*0..2].sample
+		play_array = ["rock", "paper", "scissors"]
+		@app_throw = play_array[app_guess]
+		@user_throw = params[:throw]
+
+		if @user_throw == @app_throw
+			render 'tie_rps'
+		elsif @user_throw == 'rock' && (@app_throw == 'paper' || @app_throw == 'scissors')
+			if @app_throw == 'paper'
+				render 'lose_rps'
+			else 
+				render 'win_rps'
+			end
+		elsif @user_throw == 'paper' && (@app_throw == 'rock' || @app_throw == 'scissors')
+			if @app_throw == 'scissors'
+				render 'lose_rps'
+			else 
+				render 'win_rps'
+			end
+		elsif @user_throw == 'scissors' && (@app_throw == 'rock' || @app_throw == 'paper')
+			if @app_throw == 'rock'
+				render 'lose_rps'
+			else 
+				render 'win_rps'
+			end
+		else
+			render text: 'BAD INPUT'
+		end
+	end
 
   def secret_number
   end
