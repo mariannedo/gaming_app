103dd7dc159dcb059aa1d40176c730b1dff3bd17
mariannegreat <mariannepdo@gmail.com>
2013-11-13 20:48:33 -0500

    Working secret number

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,18 @@
 class GamesController < ApplicationController
   def index
+  	@games = ["Secret Number"]
+  end
+
+  def secret_number
+  end
+
+  def secret_number_play
+  	@app_guess = [*1..5].sample
+  	@user_guess = params[:guess].to_i
+  	if @app_guess == @user_guess
+  		render 'win'
+  	else 
+  		render 'lose'
+  	end
   end
 end
